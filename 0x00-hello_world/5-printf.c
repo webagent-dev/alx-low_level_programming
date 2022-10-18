06b7d54c4c7aef24a27e000042e7de7d73a615df
webagent-dev <wizkenny019@gmail.com>
2022-10-17 18:01:38 -0700

    printf typo error fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: always 0
- *     */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
