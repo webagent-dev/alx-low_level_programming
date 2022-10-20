6947b4be05e87fa506d2292560979dce5340d028
webagent-dev <wizkenny019@gmail.com>
2022-10-19 21:26:06 -0700

    betty comment error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-  * _isalpha - main function
-  *
-  * @c: character to compare
-  *
-  * Return: 1 if true, 0 if false
-  *
-  */
+ * _isalpha - main function
+ *
+ * @c: character to compare
+ *
+ * Return: 1 if true, 0 if false
+ */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
