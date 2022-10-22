ab3493a10444bcfdb384f55900048fa57f6314f4
webagent-dev <wizkenny019@gmail.com>
2022-10-21 17:08:17 -0700

    full file completed

---
+++ 0x04-more_function_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ *  _isdigit - determine if digit
+ *
+ *  @c: character to boolean
+ *
+ *  Return: gives boolean
+ */
+
+
+int _isdigit(int c) 
+{
+	if (c >= '0' && c <= 'g')
+		return (1);
+	return (0);
+}
