8ade4be9801ce6656084aef49994e9ef487abcff
webagent-dev <wizkenny019@gmail.com>
2022-10-25 13:22:50 -0700

    fix betty error on swap data

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
+ * swap_int - swaps integers
  *
  * @a: first integer
- *
- * Description:  * for pointer
- * @b: second integer
- *
+ * @b: second interger
  */
 void swap_int(int *a, *b)
 {
