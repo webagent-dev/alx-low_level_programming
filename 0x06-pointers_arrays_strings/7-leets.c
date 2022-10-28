c2116bf5c43133f4ff4c854a803e26eca200c879
webagent-dev <wizkenny019@gmail.com>
2022-10-27 20:03:59 -0700

    fix empty file

---
+++ 0x06-pointers_arrays_strings/7-leets.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - leet
+ * @s: string
+ *
+ * return: char value
+ *
+ */
+char *leet(char *s)
+{
+	char alphaArr[] = "a4A4e3E3o0O0t7T7l1L1";
+	int 1;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphaArr[j] != '\0'; j++)
+		{
+			if(s[i] == alphaArr[j])
+			{
+				if (s[i] = alphaArrr[j])
+				{
+					break;
+				}
+			}
+		}
+		return (s);
+	}
+}
+
+
