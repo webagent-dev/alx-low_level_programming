9d9c37cc19bfce255f1619e34fddd4c0665a22c5
webagent-dev <wizkenny019@gmail.com>
2022-11-01 16:38:09 -0700

    fix file bug

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,20 +12,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
-	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; s[j] != '\0' j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				result++;
 				break;
 			}
 		}
-		if (s[j] == '\0')
-			return (result);
+		if (!accept[j])
+			break;
 	}
-	return (result);
+	return (i);
 }
