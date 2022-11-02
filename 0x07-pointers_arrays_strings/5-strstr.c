e08c5e6dc075c4f67834134aba4ebdfaa6f3ca5d
webagent-dev <wizkenny019@gmail.com>
2022-11-01 17:41:24 -0700

    fix q4 bug

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,25 +8,25 @@
  */
 char *_strstr(char *heystack, char *needle)
 {
-	int i;
-	int j;
+	unsigned int i = 0;
+	unsigned int j = 0;
 
-	if (needle[0] == '\0')
-		return (haystack);
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (heystack[i])
 	{
-		if (haystack[i] == needle[0])
+		while (needle[i] && (haystack[i] == needle[0]))
 		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (haystack + i);
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
-	return ('\0');
+	return (0);
 }
-
-
