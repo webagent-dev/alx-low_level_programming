cb08787ec49075093e361c4526e94a20cb0cbd9d
webagent-dev <wizkenny019@gmail.com>
2022-11-01 18:28:17 -0700

    q7 bug fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,13 +12,20 @@ void print_diagsums(int *a, int size)
 {
 
 	int i;
-	int diagO = 0;
-	int diagT = 0
+	int j;
+	int p;
+	int l = 0;
+	int r = 0;
 
-		for (i = 0; i < size; i++)
-		{
-			diagO += a[(i * size) + i];
-			diagT += a[(size - 1) + ((size - 1) * i)];
-		}
-	printf("%d, %d\n", diagO, diagT);
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
