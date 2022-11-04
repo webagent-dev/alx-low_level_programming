983289d6e4019f3e4691689db9f905fe9051f914
webagent-dev <wizkenny019@gmail.com>
2022-11-04 16:25:08 -0700

    Write a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vecter of arguments
+ * Return: always 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
