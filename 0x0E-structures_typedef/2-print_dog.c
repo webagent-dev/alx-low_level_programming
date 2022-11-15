63984e85ac2d2fd75daa9a096eb7b226bf236699
webagent-dev <wizkenny019@gmail.com>
2022-11-15 05:41:45 -0800

    fix typo error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,7 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
-			print("Name: %s\n", d->name);
-
+			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		if (d->name == NULL)
 			printf("Owner: (nil)\n");
